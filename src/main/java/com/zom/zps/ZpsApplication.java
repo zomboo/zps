5eab4a73724630ae310c52d9ebc660b3089878ba
zomboo <zomboo@gmail.com>
2018-04-04 08:35:48 +0800

    Initial commit

---
+++ src/main/java/com/zom/zps/ZpsApplication.java
@@ -0,0 +1,12 @@
+package com.zom.zps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpsApplication.class, args);
+	}
+}
